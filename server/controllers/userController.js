4bcfdd0f278b5078fb078099c653d78d765688bb
Azrin640 <azrin640@gmail.com>
2019-05-31 17:27:58 +0800

    Update auth flow

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -108,44 +108,44 @@ exports.authenticate = async (req, res, next) => {
     const authToken = req.body.authToken; 
 
     const authenticate = User.authenticate();
-    authenticate(req.body.email, req.body.password, async(err, user) => {
+    const authenticateUser = await authenticate(req.body.email, req.body.password)
+        .catch(error => res.json(error));
+    const user = authenticateUser.user;
 
-        if(err) res.json(err);
-        else if(!user) res.json({ status: 400, statusText: 'Email or password error.'});
-        else if(user){
+    if(user){
+        const tokenExpiry = user.authTokenExpire.getTime();
+        const now = Date.now();
 
-            const tokenExpiry = user.authTokenExpire.getTime();
-            const now = Date.now();
+        if(tokenExpiry > now){
+            const authenticated = await User.findOneAndUpdate({_id: user._id}, {authenticated: true}, {new: true, useFindAndModify: false})
+                .catch(error => res.json(error));
 
-            if(tokenExpiry < now){
-                res.json({ status: 401, statusText: 'Your authentication link has already expired. Please register again.' });
-            }
-            else if(tokenExpiry > now){
-                const authenticated = await User.findOneAndUpdate({_id: user._id}, {authenticated: true});
-                if(!authenticated) res.json({ status: 400, statusText: 'Authentication error. Please login again.' });
+            if(authenticated){
                 const token = user.generateJwt();
-                const id = user._id;
-                res.json({id, token});
+                res.json({id: user._id, token});
             }
-            else {
-                res.json({ status: 400, statusText: 'Authentication link error. Please register again.' });
-            }
-            
-        } 
-    });
+            else res.json({ status: 400, statusText: 'Authentication error. Please login again.' });
+        }
+        else res.json({ status: 401, statusText: 'Your authentication link has already expired. Please register again.' });
+    }
+    else res.json({ status: 400, statusText: 'Authentication link error. Please register again.' });
 };
 
-exports.login = (req, res) => {
+exports.login = async (req, res) => {
+
     const authenticate = User.authenticate();
-    authenticate(req.body.email, req.body.password, (err, user) => {
-        if(err) res.json(err);
-        else if(!user) res.json({ status: 400, statusText: 'Email or password error.'});
-        else if(user){
-            const token = user.generateJwt();
-            const id = user._id;
-            res.json({id, token});
-        } 
-    });
+    const authenticateUser = await authenticate(req.body.email, req.body.password)
+        .catch(error => res.json(error));
+    const user = authenticateUser.user;
+
+    if(user){
+        
+        const token = user.generateJwt();
+        res.json({id: user._id, token});
+
+    }         
+    else res.json({ status: 400, statusText: 'Email or password error.'});
+
 }
 
 // ** Forgot Password **
@@ -168,33 +168,38 @@ exports.errorsValidateForgotPassword = (req, res, next) => {
 
 exports.forgotPassword = async(req, res) => {
 
-    const user = await User.findOne({ email: req.body.email });
-
-    if(!user) res.json({ status: 400, statusText: 'Email does not exist, please register to login.'});  
-    else if(user) {
+    const user = await User.findOne({ email: req.body.email })
+        .catch(error => res.json(error));
+     
+    if(user) {
 
         const authToken = crypto.randomBytes(20).toString('hex');
         const authTokenExpire = Date.now() + 3600000; 
 
-        const updatedAuthTokenUser = await User.findOneAndUpdate({_id: user._id}, {authToken, authTokenExpire}, {new: true});
+        const updatedAuthTokenUser = await User.findOneAndUpdate({_id: user._id}, {authToken, authTokenExpire}, {new: true, useFindAndModify: false})
+            .catch(error => res.json(error));        
 
-        if(!updatedAuthTokenUser) res.json({ status: 400, statusText: 'Fail to generate token, please try again'});
+        if(updatedAuthTokenUser){
 
-        // Send email for password change                           
-        const authURL = `http://${req.headers.host}/auth/reset-password-2/${updatedAuthTokenUser.authToken}`;
-        var options = {
-            user: {
-                email: updatedAuthTokenUser.email
-            },
-            subject: 'Forgot password authentication',
-            html: `
-                <p>IMPORTANT: We have received a request from you to change your password. Please change your password in one hour after receiving this email. If it is not you, please ignore this email.</p>
-                <p>Change your password <a href="${authURL}"> this link</a></br>
-                or this link ${authURL}.</p>`
-        };
-        var sendMail = mail.send(options); 
-        res.json({ status: 200, statusText: 'Success. Please check your email to change your password in one hour before link expired.'});
-    }       
+            // Send email for password change                           
+            const authURL = `http://${req.headers.host}/auth/reset-password-2/${updatedAuthTokenUser.authToken}`;
+            var options = {
+                user: {
+                    email: updatedAuthTokenUser.email
+                },
+                subject: 'Forgot password authentication',
+                html: `
+                    <p>IMPORTANT: We have received a request from you to change your password. Please change your password in one hour after receiving this email. If it is not you, please ignore this email.</p>
+                    <p>Change your password <a href="${authURL}"> this link</a></br>
+                    or this link ${authURL}.</p>`
+            };
+            var sendMail = mail.send(options); 
+            res.json({ status: 200, statusText: 'Success. Please check your email to change your password in one hour before link expired.'});
+
+        }
+        else res.json({ status: 400, statusText: 'Fail to generate token, please try again'});
+    }
+    else res.json({ status: 400, statusText: 'Email does not exist, please register to login.'});        
 
 };
 
@@ -218,26 +223,34 @@ exports.errorsValidateResetPassword = (req, res, next) => {
 
 };
 
-exports.resetPassword = async(req, res, next) => {
+exports.resetPassword = async(req, res) => {
 
-    const user = await User.findOne({authToken: req.body.token});
-    if(!user) res.json({status: 400, statusText: 'Email or password or link error, please apply to reset your password again.'});
+    const user = await User.findOne({authToken: req.body.token})
+        .catch(error => res.json(error));
 
-    const tokenExpiry = user.authTokenExpire.getTime();
-    const now = Date.now();
+    if(user){
 
-    if(tokenExpiry < now){
-        res.json({ status: 401, statusText: 'Your authentication link has already expired. Please apply forgot password again.' });
+        const tokenExpiry = user.authTokenExpire.getTime();
+        const now = Date.now();
+
+        if(tokenExpiry > now){
+
+            const editedUser = await user.setPassword(req.body.password)
+                .catch(error => res.json(error));
+            
+            if(editedUser){
+                const jwtToken = user.generateJwt();
+                const id = editedUser._id;
+                res.json({status: 200, statusText: 'Password reset successful.', id, jwtToken});
+            }
+            else res.json({ status: 401, statusText: 'Error resetting password, please try again.' });
+            
+        }
+        else res.json({ status: 401, statusText: 'Your authentication link has already expired. Please re apply forgot password again.' });
+
     }
-    else if(tokenExpiry > now){
-        const editedUser = await user.setPassword(req.body.password);
-        if(!editedUser) res.json({status: 400, statusText: 'Email or password error, please use the same email to reset your password.'});
-        else if(editedUser) {
-            const jwtToken = user.generateJwt();
-            const id = editedUser._id;
-            res.json({status: 200, statusText: 'Password reset successful.', id, jwtToken});
-        }            
-    }
+    else res.json({status: 400, statusText: 'Email or password or link error, please apply to reset your password again.'});
+
 };
 
 
